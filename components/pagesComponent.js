0c82c6d15fa48226bbd3e255b1a0c89d225052a8
med <izekki.mohamed@gmail.com>
2022-05-23 23:56:59 +0200

    remove pagination

--- components/pagesComponent.js
+++ components/pagesComponent.js
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { PagesContainerStyles } from "/styles/PagesContainerStyles";
-import { PaginationStyles } from "../styles/paginationStyles";
 import CardsList from "./CardsList";
 
 function PagesContainer({ section }) {
@@ -8,14 +7,6 @@ function PagesContainer({ section }) {
   return (
     <PagesContainerStyles>
       <h3>{section} </h3>
-      <PaginationStyles className="pagenation">
-        <li>1</li>
-        <li className={`${currentPage === 1 ? "active" : ""}`}>2</li>
-        <li>3</li>
-        <li>4</li>
-        <li>5</li>
-        <li>{"->"}</li>
-      </PaginationStyles>
       <CardsList />
       <CardsList />
       <CardsList />
