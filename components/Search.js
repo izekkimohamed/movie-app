af13c26500968c6d21c21b1a29ae910829250197
med <izekki.mohamed@gmail.com>
2022-05-29 15:33:29 +0200

    usecallback

--- components/Search.js
+++ components/Search.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useRouter } from "next/router";
 import { useCallback, useEffect, useState } from "react";
 import { FaSearch } from "react-icons/fa";
